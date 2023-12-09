8ebb2b45b1f9c0e49df28c4e3eb874147a142ee6
Ramona Marti <ramona.marti@students.fhnw.ch>
2023-12-09 19:43:22 +0100

    udate svgs

--- experimental/js/choiceInputProjector.js
+++ experimental/js/choiceInputProjector.js
@@ -55,21 +55,18 @@ const projectChoiceInput = (timeout) => (inputController, formCssClassName) => {
     `)[0];
     const [svgClear, svgClose, svgOpen] = dom(`
         <div class="clear" id="clear">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-                <path d="M5 5.00002L15.6452 15.6452M5 15.6452L15.6452 5" stroke="#A0A3BD" stroke-width="1.66667"
-                    stroke-linecap="round" stroke-linejoin="round" />
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="none">
+                <path d="M 5 5  L 15 15  M 5 15  L 15 5" stroke="black" stroke-width="2" stroke-linecap="round" />
             </svg>
         </div>
         <div class="icon close show">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                <path d="M5 9.03658L11.9632 15.9998L18.9263 9.03658" stroke="#A0A3BD" stroke-width="2"
-                    stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M 5 9 L 12 16 L 19 9" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
             </svg>
         </div>
         <div class="icon open">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                <path d="M5 16L11.9632 8.96317L18.9263 16" stroke="#A0A3BD" stroke-width="2"
-                    stroke-linecap="round" stroke-linejoin="round" />
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none">
+                <path d="M 5 16 L 12 9 L 19 16" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
             </svg>
         </div>
     `);
