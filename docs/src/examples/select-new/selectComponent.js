e353afcb7771bf96c2244421a133733773d1fce7
Ramona Marti <ramona.marti@students.fhnw.ch>
2024-07-10 16:53:27 +0200

    temporary: add file for old user testers

---
+++ docs/src/examples/select-new/selectComponent.js
@@ -0,0 +1,8 @@
+import { SelectComponentByCallbacks, pageCss } from "https://fhnw-ramonamarti.github.io/Kolibri/src/kolibri/projector/selectComponent/selectComponent.js";
+
+export {
+    SelectComponentByCallbacks as SelectComponent, pageCss
+};
+
+// There were uptades since the user test was released
+// The files are placed in an new location: https://fhnw-ramonamarti.github.io/Kolibri/src/kolibri/projector/selectComponent/
