81bd2d6513ce67c3a8adcbc652fc48880340c1c0
Ramona Marti <ramona.marti@students.fhnw.ch>
2024-07-10 17:28:21 +0200

    temp: remove not working for user tests

--- docs/src/examples/select-new/selectComponent.js
+++
@@ -1,8 +0,0 @@
-import { SelectComponentByCallbacks, pageCss } from "https://fhnw-ramonamarti.github.io/Kolibri/src/kolibri/projector/selectComponent/selectComponent.js";
-
-export {
-    SelectComponentByCallbacks as SelectComponent, pageCss
-};
-
-// There were uptades since the user test was released
-// The files are placed in an new location: https://fhnw-ramonamarti.github.io/Kolibri/src/kolibri/projector/selectComponent/
